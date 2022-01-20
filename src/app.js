5e0b95fb013c356cf211248d36140eda094f57cd
xguhx <gmartinez-de-oliveir@myseneca.ca>
2022-01-20 17:30:13 -0500

    Adding Router

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const compression = require('compression');
 
 // Version from our package.json file
-const { version } = require('../package.json');
+//const { version } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -29,22 +29,8 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    // Update this info with your own details...
-    author: 'David Humphrey',
-    githubUrl: 'https://github.com/humphd/fragments',
-    version,
-  });
-});
+// Define our routes
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found can't be found
 app.use((req, res) => {
