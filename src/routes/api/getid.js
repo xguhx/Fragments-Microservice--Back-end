18b8a3733c634afd40e6c3a6330bff04d1aeac62
xguhx <gmartinez-de-oliveir@myseneca.ca>
2022-03-04 21:15:22 -0500

    Fixing a1

--- src/routes/api/getid.js
+++ src/routes/api/getid.js
@@ -1,11 +1,18 @@
-// src/routes/api/get.js
+// src/routes/api/getid.js
 const { Fragment } = require('../../model/fragment');
 /**
  * Get a list of fragments for the current user
  */
 
 module.exports = async (req, res) => {
-  let fragment = await Fragment.byId(req.user, req.params.id);
-
-  res.status(200).json(fragment);
+  //Parse Data
+  let fragment;
+  let data;
+  try {
+    fragment = await Fragment.byId(req.user, req.params.id);
+    data = fragment.getData();
+  } catch (err) {
+    res.status(400).json('Error requesting fragment');
+  }
+  res.status(200).json(data);
 };
