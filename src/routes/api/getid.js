2d1c6c0807f60c9f762aca9c2dab9b45a3a10386
xguhx <gmartinez-de-oliveir@myseneca.ca>
2022-03-04 21:33:48 -0500

    Fixing Get:id

--- src/routes/api/getid.js
+++ src/routes/api/getid.js
@@ -8,11 +8,15 @@ module.exports = async (req, res) => {
   //Parse Data
   let fragment;
   let data;
+  //let buffer;
   try {
     fragment = await Fragment.byId(req.user, req.params.id);
-    data = fragment.getData();
+
+    data = await fragment.getData();
+
+    // buffer = Buffer.from(data, 'utf-8');
   } catch (err) {
     res.status(400).json('Error requesting fragment');
   }
-  res.status(200).json(data);
+  res.status(200).json(data.toString('utf-8'));
 };
